4bef8f651a57acb6303ee6b100ca6ef9dc503ecd
Werner Hurter <werner@omnisient.com>
2021-07-07 15:43:30 +0200

    Add project files.

---
+++ SBReceiver/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Azure.ServiceBus;
+using SBShared.Models;
+using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SBReceiver
+{
+    class Program
+    {
+        const string connectionString = "Endpoint=sb://omnisientservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=JnCkGx0zy1cYYz+zDcBUd13+cYOpCHTcakQptsd7fQY=";
+        const string queueName = "personqueue";
+        static IQueueClient queueClient;
+
+
+        static async Task Main(string[] args)
+        {
+            queueClient = new QueueClient(connectionString, queueName);
+
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+
+            Console.ReadLine();
+            await queueClient.CloseAsync();
+        }
+
+        private static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            var jsonString = Encoding.UTF8.GetString(message.Body);
+            PersonModel person = JsonSerializer.Deserialize<PersonModel>(jsonString);
+            Console.WriteLine($"Person Received: {person.FirstName} { person.LastName}");
+
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        private static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs arg)
+        {
+            Console.WriteLine($"Message handler exception: {arg.Exception}");
+            return Task.CompletedTask;
+        }
+    }
+}
